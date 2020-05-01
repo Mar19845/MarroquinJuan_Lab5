911de743cd58fb463cc101f7de1e25abccf8cd10
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-01 01:13:46 -0600

    agregar sonido de muerte

--- MarroquinJuan_Lab5/Assets/FPS/Target.cs
+++ MarroquinJuan_Lab5/Assets/FPS/Target.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class Target : MonoBehaviour
 {
     public float health  = 50;
+    public float time;
+    private AudioSource sound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,12 +24,13 @@ public class Target : MonoBehaviour
         health -= amount;
         if(health<= 0)
         {
+            sound.Play();
+            Contador.scoreval += 10;
             Morir();
         }
     }
     void Morir()
     {
-        Destroy(gameObject);
-        Contador.scoreval += 10;
+        Destroy(gameObject,time);
     }
 }
